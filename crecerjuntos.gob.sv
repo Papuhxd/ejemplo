2cd26e726ca249340838f9bb0e39bac0abd7ad91
Papuhxd <137443132+Papuhxd@users.noreply.github.com>
2023-06-22 16:05:48 -0300

    Create crecerjuntos.gob.sv

---
+++ crecerjuntos.gob.sv
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title>https://crecerjuntos.gob.sv/validate/certification/*H3T54P143130B5F79A5F7526437428G8S5D9C5F9</title>
+</head>
+<body bgcolor="#ebebeb">
+<img src="images/verificacion.png" width="1340" height "600" align=middle">
